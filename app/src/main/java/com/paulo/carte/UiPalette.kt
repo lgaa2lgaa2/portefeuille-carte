ee04af635e7bbab016606b025564712ef0ec8fe3
lgaa2lgaa2 <49477279+lgaa2lgaa2@users.noreply.github.com>
2026-08-31 21:45:50 +0200

    feat: add wallet v2 visual palette

---
+++ app/src/main/java/com/paulo/carte/UiPalette.kt
@@ -0,0 +1,15 @@
+package com.paulo.carte
+
+import android.graphics.Color
+
+object UiPalette {
+    val Background = Color.rgb(4, 15, 28)
+    val Surface = Color.rgb(10, 31, 51)
+    val SurfaceAlt = Color.rgb(15, 45, 72)
+    val Gold = Color.rgb(242, 190, 74)
+    val GoldSoft = Color.rgb(255, 213, 120)
+    val Text = Color.WHITE
+    val Muted = Color.rgb(178, 192, 207)
+    val Danger = Color.rgb(150, 49, 37)
+    val AccentBlue = Color.rgb(15, 93, 203)
+}
